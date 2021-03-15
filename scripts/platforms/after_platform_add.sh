096aa94b4a9fcbf69924ac045566a24642641415
denizz <workdenizz@gmail.com>
2021-03-15 19:18:18 +0700

    Quasar added.

--- scripts/platforms/after_platform_add.sh
+++ scripts/platforms/after_platform_add.sh
@@ -2,8 +2,17 @@
 
 cd -- "$(dirname "$BASH_SOURCE")"
 
-cd ../../../waysofhistory-icons
+cd ../../quasar-icongenie
+
+windows() { [[ -n "$WINDIR" ]]; }
+
+if windows
+then
+    cmd <<< "mklink /j src-cordova \"../../waysofhistory\"" > /dev/null
+else
+    ln -s "src-cordova" "../../waysofhistory"
+fi
 
 icongenie generate -m cordova -i src-cordova/res/icon.png --theme-color 000
 
-#read -p "Press enter to continue..."
+read -p "Press enter to continue..."
