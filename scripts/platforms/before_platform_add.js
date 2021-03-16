8f5fdb5f57ff57d916bdbf723af5fe7658462306
denizz <workdenizz@gmail.com>
2021-03-16 20:51:55 +0700

    + before_platform_add script modified from sh to js

--- scripts/platforms/before_platform_add.js
+++ scripts/platforms/before_platform_add.js
@@ -6,13 +6,13 @@ const {utils} = require('jjbox-utils');
 	const 	argv = utils.prepareArgv(process.argv),
 			argp = utils.prepareArgp(argv);
 	
-	let configText = utils.readFile('../../config.xml');
+	let configText = utils.readFile('config.xml');
 	
-	utils.log('<--- start clear config icon and splash  --->');
+	utils.log('<--- starting cleaning config icon and splash tags --->');
 	
 	configText = configText.replace(/<(icon|splash).+?\/>[\r\n\t ]*/g, '');
 	
-	utils.writeFile('../../config.xml', configText);
+	utils.writeFile('config.xml', configText);
 	
 	utils.log('<--- done --->');
 })();
