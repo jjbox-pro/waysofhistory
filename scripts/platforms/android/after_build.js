47342adb3e5811326ff9b7fadb7056c079bb84be
denizz <workdenizz@gmail.com>
2021-04-12 17:54:44 +0700

    + +

--- scripts/platforms/android/after_build.js
+++ scripts/platforms/android/after_build.js
@@ -5,5 +5,8 @@ module.exports = function(ctx) {
 	let buildType = ctx.opts.options.release ? 'Release' : 'Debug'
 		command = 'gradlew :app:bundle' + buildType;
 	
+	if( process.platform !== 'win32' )
+		command = './' + command;
+	
 	child_process.execSync(command, {cwd: 'platforms/android', stdio: 'inherit'});
 };
